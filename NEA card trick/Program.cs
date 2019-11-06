726ed89effe1b31ccdb2d11c6d88260df9857b45
Nathan Smith <K036659@eltham-college.org.uk>
2019-11-06 12:51:06 +0000

    Add project files.

---
+++ NEA card trick/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NEA_card_trick
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            string[,] Piles = new string[3, 7];
+            string[] Houses = new string[3] { "CLUBS", "HEARTS", "SPADES" };
+            List<string> Deck = new List<string>();
+            //for (int i = 1; i <= 21; i++) Deck.Add(string.Format("{0} of {1}", i % 21, Houses[]);
+            for (int x = 0; x < 3; x++)
+            {
+                for (int y = 0; y < 7; y++)
+                {
+                }
+            }
+        }
+    }
+}
